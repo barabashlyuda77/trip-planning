2213256793aec965c35480b28b4aa94ae166d0cb
Lyuda Barabash <barabashlyuda@gmail.com>
2019-04-07 11:29:48 +0700

    Create addcomponent

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './home.scss';
 
 import Search from '../search/search.js';
+import Add from '../add/add.js';
 
 class home extends Component {
   render() {
     return (
       <div className="home">
         <Search />
-        <p>Add new trip component</p>
+        <Add />
         <p>Wrapper for the list ot nothing found components</p>
       </div>
     );
