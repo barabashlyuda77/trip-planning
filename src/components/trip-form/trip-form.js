92291551884ab0fecced8c71daef03492b4508af
Lyuda Barabash <barabashlyuda@gmail.com>
2019-04-21 12:10:19 +0700

    Add validation to form

--- src/components/trip-form/trip-form.js
+++ src/components/trip-form/trip-form.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './trip-form.scss';
 
+const regex = /^[a-zA-Z][a-zA-Z ]*[a-zA-Z]$/;
+
 class tripForm extends Component {
   state = {
     country: '',
-    city: ''
+    city: '',
+    countryError: '',
+    cityError: ''
   };
 
   handlerChangeCountry = (event) => {
@@ -15,19 +19,48 @@ class tripForm extends Component {
     this.setState({ city: event.target.value})
   }
 
+  validateCountry = () => {
+    const { country } = this.state;
+    const isInputValid = regex.test(country);
+
+    this.setState({
+      countryError:
+      isInputValid ? null : 'Country name is not valid'
+    });
+
+    return isInputValid;
+  }
+
+  validateCity = () => {
+    const { city } = this.state;
+    const isInputValid = regex.test(city);
+
+    this.setState({
+      cityError:
+      isInputValid ? null : 'City name is not valid'
+    });
+
+    return isInputValid;
+  }
+
   redirectHome = () => {
     this.props.history.push('/');
   }
 
-  inputClickHandler = () => {
-    fetch('http://localhost:8000/add-data-to-db/', {
-      headers: { 'Content-Type': 'application/json; charset=utf-8' },
-      method: 'POST',
-      body: JSON.stringify({
-        country: this.state.country,
-        city: this.state.city
-      })
-    });
+  handledAdd = () => {
+    const isCountryValid = this.validateCountry();
+    const isCityValid = this.validateCity();
+
+    if (isCountryValid && isCityValid) {
+      fetch('http://localhost:8000/add-data-to-db/', {
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        method: 'POST',
+        body: JSON.stringify({
+          country: this.state.country,
+          city: this.state.city
+        })
+      });
+    }
   }
 
   render() {
@@ -37,20 +70,22 @@ class tripForm extends Component {
         <form>
           <label>Destination Country</label>
           <input
-             required="required"
             type="text"
             placeholder="Italy"
             value={this.state.country}
             onChange={this.handlerChangeCountry}
+            className={`form-control ${this.state.countryError ? 'is-invalid' : ''}`}
           />
+          <div className='invalid-feedback'>{this.state.countryError}</div>
           <label>Destination City</label>
           <input
-            required={true}
             type="text"
             placeholder="Rome"
             value={this.state.city}
             onChange={this.handlerChangeCity}
+            className={`form-control ${this.state.cityError ? 'is-invalid' : ''}`}
           />
+          <div className='invalid-feedback'>{this.state.cityError}</div>
           <div>
             <input
               onClick={this.redirectHome}
@@ -59,7 +94,7 @@ class tripForm extends Component {
               value="Cancel"
             />
             <input
-              onClick={this.inputClickHandler}
+              onClick={this.handledAdd}
               className="submit-button"
               type="button"
               value="Add Trip"
