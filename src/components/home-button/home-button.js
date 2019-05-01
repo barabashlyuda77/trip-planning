65340c61183ca1b8c7a3a81ac1705102ecceb44e
Lyuda Barabash <barabashlyuda@gmail.com>
2019-05-01 14:05:07 +0700

    Refactor home button

--- src/components/home-button/home-button.js
+++ src/components/home-button/home-button.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 
 import './home-button.scss';
 
-class homeButton extends Component {
-  render() {
-    return (
-      <div className="home-wrapper">
-        <Link to="/">
-          <img
-            alt="home button"
-            src="https://img.icons8.com/material/24/000000/home-page.png"
-          />
-          <p>Trip planing</p>
-        </Link>
-      </div>
-    );
-  }
+const homeButton = () =>{
+  return (
+    <div className="home-wrapper">
+      <Link to="/">
+        <img
+          alt="home button"
+          src="https://img.icons8.com/material/24/000000/home-page.png"
+        />
+        <p>Trip planing</p>
+      </Link>
+    </div>
+  );
 }
 
 export default homeButton;
