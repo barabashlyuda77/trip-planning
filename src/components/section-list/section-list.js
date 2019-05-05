8555d83e18d4a5c8542fb139a326a11d124bad1f
Lyuda Barabash <barabashlyuda@gmail.com>
2019-05-05 10:42:57 +0700

    Add read more/less functionality

--- src/components/section-list/section-list.js
+++ src/components/section-list/section-list.js
@@ -1,37 +1,85 @@
 import React, {Component} from 'react';
 import './section-list.scss';
 
-const listOfItems = [{
-  name: 'Big Budda',
-  details: 'Open every day. Open every day.'
-}]
+const listOfItems = [
+  {
+    name: 'Big Budda',
+    details: 'Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day.'
+  },
+  {
+    name: 'Big Budda2',
+    details: 'Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day. Open every day.'
+  }
+]
 
 class sectionList extends Component {
+  constructor(props) {
+    super(props);
+    const readMoreState = {};
+    listOfItems.forEach((key) => {
+      readMoreState[key.name] = 'close';
+    });
+    this.state = {
+      readMoreState
+    }
+  }
+
+  headnlerReadMore = (key) => {
+    const newState = this.state;
+    newState.readMoreState[key] = 'open';
+    this.setState(newState);
+  }
+
+  headnlerReadLess = (key) => {
+    const newState = this.state;
+    newState.readMoreState[key] = 'close';
+    this.setState(newState);
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="section-list-wrapper">
-        <div className="item-wrapper">
-          <div className="text-wrapper">
-            <h4>Big Budda</h4>
-            <p>Open every day. Open every day.Open every day.Open every day.Open every day.Open every day.Open every day.Open every day.Open every day.Open every day.Open every day.Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. Open every day.Open every day. </p>
+        {listOfItems.map((item) => {
+          const maxLengthOfText = 80;
+          const detailsPart1 = item.details.substring(0, maxLengthOfText);
+          const detailsPart2 = item.details.substring(maxLengthOfText);
+
+          return <div className="item-wrapper" key={item.name}>
+            <div className="text-wrapper">
+              <h4>{item.name}</h4>
+              <p>
+                <span className="details-part1">{detailsPart1}</span>
+                {
+                  this.state.readMoreState[item.name] === 'open'
+                  ? null
+                  : <span>
+                      <span className="dots">... </span>
+                      <span className="read-more" onClick={() => this.headnlerReadMore(item.name)}>
+                        read more
+                      </span>
+                    </span>
+                }
+                {
+                  this.state.readMoreState[item.name] === 'close'
+                  ? null
+                  : <span>
+                      <span className="details-part2">{detailsPart2}</span>
+                      <span className="read-less" onClick={() => this.headnlerReadLess(item.name)}>
+                        read less
+                      </span>
+                    </span>
+                }
+
+              </p>
+            </div>
+            <img
+              className="img-delete"
+              alt="delete"
+              src="https://img.icons8.com/small/32/000000/delete-sign.png"
+            />
           </div>
-          <img
-            className="img-delete"
-            alt="delete"
-            src="https://img.icons8.com/small/32/000000/delete-sign.png"
-          />
-        </div>
-        <div className="item-wrapper">
-          <div className="text-wrapper">
-            <h4>Big Budda</h4>
-            <p>Nice place</p>
-          </div>
-          <img
-            className="img-delete"
-            alt="delete"
-            src="https://img.icons8.com/small/32/000000/delete-sign.png"
-          />
-        </div>
+        })}
       </div>
     );
   }
--- src/components/section/section.js
+++ src/components/section/section.js
@@ -5,6 +5,35 @@ import Back from '../back/back.js';
 import SectionList from '../section-list/section-list.js';
 
 class tripDetails extends Component {
+  state = {
+    name: '',
+    details: ''
+  }
+
+  handerChangeName = (event) => {
+    this.setState({ name: event.target.value})
+  }
+
+  handerChangeDetails = (event) => {
+    this.setState({ details: event.target.value})
+  }
+
+  // addDataToDb = ({ name, details }) => {
+  //   return fetch('http://localhost:8000/add-section-details-to-db/', {
+  //     headers: { 'Content-Type': 'application/json; charset=utf-8' },
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       name,
+  //       details
+  //     })
+  //   });
+  // }
+
+  handledAdd = () => {
+    // this.addDataToDb(this.state)
+    console.log('add', this.state);
+  }
+
   render() {
     return (
       <div className="component-wrapper">
@@ -17,16 +46,21 @@ class tripDetails extends Component {
                 className="name input-info"
                 type="text"
                 placeholder="Name"
+                value={this.state.name}
+                onChange={this.handerChangeName}
               />
               <input
                 className="details input-info"
                 type="text"
                 placeholder="Details..."
+                value={this.state.details}
+                onChange={this.handerChangeDetails}
               />
               <input
                 className="add-button"
                 type="button"
                 value="add"
+                onClick={this.handledAdd}
               />
             </form>
           </div>
