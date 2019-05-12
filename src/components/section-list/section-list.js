c6b18f1d7abd176fe265bc61875e692a343b0772
Lyuda Barabash <barabashlyuda@gmail.com>
2019-05-12 11:34:42 +0800

    Refactor section list component

--- src/components/section-list/section-list.js
+++ src/components/section-list/section-list.js
@@ -40,65 +40,74 @@ class sectionList extends Component {
     this.setState(newState);
   }
 
+  showAllItem = (item) => {
+    return <div className="item-wrapper" key={item.name}>
+      <div className="text-wrapper">
+        <h4>{item.name}</h4>
+        <p>{item.details}</p>
+      </div>
+      <img
+        className="img-delete"
+        alt="delete"
+        src="https://img.icons8.com/small/32/000000/delete-sign.png"
+      />
+    </div>
+  }
+
+  getDetails = (item) => {
+    const { name, details} = item;
+
+    const maxLengthOfText = 87;
+
+    if (details.length < maxLengthOfText) {
+      return details;
+    }
+
+    const detailsPart1 = details.substring(0, maxLengthOfText);
+    const detailsPart2 = details.substring(maxLengthOfText);
+
+    return <div>
+        <span className="details-part1">{detailsPart1}</span>
+        {
+          this.state.readMoreState[name] === 'open'
+          ? null
+          : <span>
+              <span className="dots">... </span>
+              <span className="read-more" onClick={() => this.headnlerReadMore(name)}>
+                read more
+              </span>
+            </span>
+        }
+        {
+          this.state.readMoreState[name] === 'close'
+          ? null
+          : <span>
+              <span className="details-part2">{detailsPart2} </span>
+              <span className="read-less" onClick={() => this.headnlerReadLess(name)}>
+                read less
+              </span>
+            </span>
+      }
+    </div>
+  }
+
   render() {
     return (
       <div className="section-list-wrapper">
         {this.state.listOfItems.length === 0
           ? null
           : this.state.listOfItems.map((item) => {
-            const maxLengthOfText = 87;
-
-            if (item.details.length < maxLengthOfText) {
-              return <div className="item-wrapper" key={item.name}>
-                <div className="text-wrapper">
-                  <h4>{item.name}</h4>
-                  <p>{item.name}</p>
-                </div>
-                <img
-                  className="img-delete"
-                  alt="delete"
-                  src="https://img.icons8.com/small/32/000000/delete-sign.png"
-                />
-              </div>
-            }
-
-            const detailsPart1 = item.details.substring(0, maxLengthOfText);
-            const detailsPart2 = item.details.substring(maxLengthOfText);
-
             return <div className="item-wrapper" key={item.name}>
               <div className="text-wrapper">
                 <h4>{item.name}</h4>
-                <p>
-                  <span className="details-part1">{detailsPart1}</span>
-                  {
-                    this.state.readMoreState[item.name] === 'open'
-                    ? null
-                    : <span>
-                        <span className="dots">... </span>
-                        <span className="read-more" onClick={() => this.headnlerReadMore(item.name)}>
-                          read more
-                        </span>
-                      </span>
-                  }
-                  {
-                    this.state.readMoreState[item.name] === 'close'
-                    ? null
-                    : <span>
-                        <span className="details-part2">{detailsPart2} </span>
-                        <span className="read-less" onClick={() => this.headnlerReadLess(item.name)}>
-                          read less
-                        </span>
-                      </span>
-                }
-
-              </p>
+                <p>{this.getDetails(item)}</p>
+              </div>
+              <img
+                className="img-delete"
+                alt="delete"
+                src="https://img.icons8.com/small/32/000000/delete-sign.png"
+              />
             </div>
-            <img
-              className="img-delete"
-              alt="delete"
-              src="https://img.icons8.com/small/32/000000/delete-sign.png"
-            />
-          </div>
         })}
       </div>
     );
